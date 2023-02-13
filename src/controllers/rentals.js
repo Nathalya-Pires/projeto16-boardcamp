6c6ce63267d39a6ef100976088d03f768521d6eb
Nathalya Ohana Pires Ferreira <nathyohana@gmail.com>
2023-02-13 15:41:55 -0400

    atualizações

--- src/controllers/rentals.js
+++ src/controllers/rentals.js
@@ -4,17 +4,12 @@ import dayjs from "dayjs";
 export async function listRentals(req, res) {
   try {
     const rentals = await db.query(
-      /*  `SELECT rentals.* 'customer', json_build_object('id', customers.id, 'name', customers.name) 'game' json_build_object ('id', games.id, 'name', games.name ) FROM rentals 
+      `SELECT rentals.*, json_build_object('id', customers.id, 'name', customers.name) AS customer, json_build_object ('id', games.id, 'name', games.name ) AS GAME FROM rentals 
       JOIN customers
       ON "customerId" = customers.id
       JOIN games
-      ON "gameId" = games.id;` */
+      ON "gameId" = games.id;` 
 
-      `SELECT json_build_object(rentals.*, 'customer', json_build_object( 'id', customers.id, 'name', customers.name, 'game', json_build_object('id', games.id, 'name', games.name))) FROM rentals 
-      JOIN customers
-      ON "customerId" = customers.id
-      JOIN games
-      ON "gameId" = games.id;`
     );
 
     res.send(rentals.rows);
@@ -28,26 +23,47 @@ export async function addRentals(req, res) {
   let dateSave = dayjs().format("YYYY-MM-DD");
 
   try {
+    
     const customerOk = await db.query(`SELECT * FROM customers WHERE id = $1`, [
       customerId,
     ]);
-
-    if (!customerOk) return res.sendStatus(400);
-
+    
+    if (customerOk.rowCount === 0) return res.sendStatus(400);
+    
     const gameOk = await db.query(`SELECT * FROM games WHERE id = $1`, [
       gameId,
     ]);
 
-    if (!gameOk) return res.sendStatus(400);
-
-    let rentDate = dateSave;
-    let originalPrice = Number(daysRented) * gameOk.rows[0].pricePerDay;
-
-    const insertGames = await db.query(
-      `INSERT INTO rentals ("customerId", "gameId", "rentDate", daysRented", "returnDate", "originalPrice", "delayFee" ) VALUES ($1,$2,$3,$4,$5,$6,$7);`,
-      [customerId, gameId, rentDate, daysRented, null, originalPrice, null ]
+    if (gameOk.rowCount === 0) return res.sendStatus(400);
+    
+    const rentedOk = await db.query(
+      `SELECT * FROM rentals WHERE "gameId" = $1`,
+      [gameId]
     );
 
+    if (rentedOk.rowCount === gameOk.rows[0].stockTotal)
+      return res.sendStatus(400);
+    
+    if (daysRented <= 0) return res.sendStatus(400);
+    
+    let rentDate = dateSave;
+    let originalPrice = Number(daysRented) * gameOk.rows[0].pricePerDay;
+    let returnDate = null;
+    let delayFee = null;
+    
+    const insertGames = await db.query(
+      `INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee" ) VALUES ($1,$2,$3,$4,$5,$6,$7);`,
+      [
+        customerId,
+        gameId,
+        rentDate,
+        daysRented,
+        returnDate,
+        originalPrice,
+        delayFee,
+      ]
+    );
+   
     console.log(insertGames.rows);
 
     res.sendStatus(201);
