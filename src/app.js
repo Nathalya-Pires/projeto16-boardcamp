3d6abb10d39c1eae018730fc172930ad1d4a2872
Nathalya Ohana Pires Ferreira <nathyohana@gmail.com>
2023-02-12 21:51:53 -0400

    build: complemento de libs e configs

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-app.js
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+// app.use(routers);
+
+const port = process.env.PORT;
+app.listen(port, () => console.log(`Servidor rodando na porta: ${port}`));
