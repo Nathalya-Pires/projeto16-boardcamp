e6718a627d112b1962c11d2097c433fa15f8f430
Nathalya Ohana Pires Ferreira <nathyohana@gmail.com>
2023-02-13 16:48:04 -0400

    atualizaçoes schema

--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { addGames, listGames } from "../controllers/games.js";
+import { confirmSchema } from "../middlewares/confirmSchema.js";
+import { gameSchema } from "../schemas/games.schema.js";
 
 const gameRoutes = Router();
 
-gameRoutes.get("/games", listGames);
-gameRoutes.post("/games", addGames);
+gameRoutes.get("/games", confirmSchema(gameSchema), listGames);
+gameRoutes.post("/games", confirmSchema(gameSchema), addGames);
 
 export default gameRoutes;
